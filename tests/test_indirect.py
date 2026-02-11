ab299679f29752d9309387319ae4bab003bccbf9
aleksandr kuznetsov <anarond@gmail.com>
2026-02-11 12:33:43 +0300

    add test_indirect.py

---
+++ tests/test_indirect.py
@@ -0,0 +1,17 @@
+import pytest
+from selene import browser, have
+
+@pytest.mark.parametrize(
+    "window_size",
+    [(1366, 768), (1600, 900)],
+    indirect=True
+)
+@pytest.mark.parametrize(
+    "browser_name",
+    [pytest.param('chrome'), pytest.param('firefox', marks=pytest.mark.skip)],
+    indirect=True
+)
+def test_override_window_size_indirectly(desktop_browser):
+    browser.open("https://github.com/")
+    browser.element(".HeaderMenu-link--sign-in").click()
+    browser.should(have.url_containing("/login"))
