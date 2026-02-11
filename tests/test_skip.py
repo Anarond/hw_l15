f8f5489e5edc315fc6db91e1fa70f591010f1aef
aleksandr kuznetsov <anarond@gmail.com>
2026-02-11 12:22:34 +0300

    add test_skip.py

---
+++ tests/test_skip.py
@@ -0,0 +1,23 @@
+import pytest
+from selene import have, by
+
+
+def test_github_desktop(desktop_browser):
+    browser = desktop_browser
+    if browser.config.driver_name == 'firefox':
+        pytest.skip("Пропускаем тест для Firefox")
+    browser.open("https://github.com/")
+
+    browser.element(".HeaderMenu-link--sign-in").click()
+    browser.should(have.url_containing("/login"))
+
+
+def test_github_mobile(mobile_browser):
+    browser = mobile_browser
+    if browser.config.driver_name == 'firefox':
+        pytest.skip("Пропускаем тест для Firefox")
+    browser.open("https://github.com/")
+
+    browser.element(".flex-lg-row.flex-items-center button[type='button']").click()
+    browser.element(by.text("Sign in")).click()
+    browser.should(have.url_containing("/login"))
